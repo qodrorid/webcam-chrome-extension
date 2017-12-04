22857dffa474431387caefe426fbc5c06437f7ca
agus nurwanto <agusnurwantomuslim@gmail.com>
2017-12-04 17:33:42 +0700

    fixing error variable

--- camera.js
+++ camera.js
@@ -15,7 +15,6 @@ var PHOTO_WIDTH = 960;
 var mediaSource = new MediaSource();
 mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
 var mediaRecorder;
-var recordedBlobs;
 var sourceBuffer;
 var intervalTimer;
 var startTime;
@@ -23,7 +22,6 @@ var finishTime;
 
 
 var gumVideo = document.querySelector('video#gum');
-var $finishBtn = document.querySelector('#finish');
 var canvas = document.querySelector('#canvas');
 
 
@@ -95,95 +93,4 @@ function handleDataAvailable(event) {
     recordedBlobs.push(event.data);
   }
 }
-
-
-// The nested try blocks will be simplified when Chrome 47 moves to Stable
-function startRecording() {
-  var options = {
-    audioBitsPerSecond : 128000,
-    videoBitsPerSecond : 250000,
-    mimeType: 'video/webm',
-  };
-  recordedBlobs = [];
-  try {
-    mediaRecorder = new MediaRecorder(window.stream, options);
-  } catch (e0) {
-    console.log('Unable to create MediaRecorder with options Object: ', e0);
-    try {
-      options = {
-        audioBitsPerSecond : 128000,
-        videoBitsPerSecond : 2500000,
-        mimeType: 'video/webm,codecs=vp9',
-      };
-      mediaRecorder = new MediaRecorder(window.stream, options);
-    } catch (e1) {
-      console.log('Unable to create MediaRecorder with options Object: ', e1);
-      try {
-        options = 'video/vp8'; // Chrome 47
-        mediaRecorder = new MediaRecorder(window.stream, options);
-      } catch (e2) {
-        alert('MediaRecorder is not supported by this browser.\n\n' + 'Try Firefox 29 or later, or Chrome 47 or later, with Enable experimental Web Platform features enabled from chrome://flags.');
-        console.error('Exception while creating MediaRecorder:', e2);
-        return;
-      }
-    }
-  }
-  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-  // recordButton.textContent = 'Stop Recording';
-  // playButton.disabled = true;
-  // downloadButton.disabled = true;
-  mediaRecorder.onstop = handleStop;
-  mediaRecorder.ondataavailable = handleDataAvailable;
-  mediaRecorder.start(10); // collect 10ms of data
-
-  startTimer();
-
-  console.log('MediaRecorder started', mediaRecorder);
-}
-
-
-function takeSnapshot() {
-  console.log('take snapshot pressed');
-
-  upload();
-}
-
-
-function upload() {
-  var width = PHOTO_WIDTH;
-  var height = gumVideo.videoHeight / (gumVideo.videoWidth/width);
-
-  canvas.setAttribute('width', width);
-  canvas.setAttribute('height', height);
-
-  var context = canvas.getContext('2d');
-  canvas.width = width;
-  canvas.height = height;
-  context.drawImage(gumVideo, 0, 0, width, height);
-
-  var dataUrl = canvas.toDataURL('image/png');
-
-  chrome.runtime.getBackgroundPage( (backgroundPage) => {
-    backgroundPage.uploadDataUrl(dataUrl);
-    window.close();
-  });
-}
-
-
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.id == 'close-camera-window') {
-    console.log('close-camera-window');
-    window.close();
-  }
-});
-
-
-function init() {
-  $finishBtn.addEventListener('click', function() {
-    takeSnapshot();
-  });
-}
-
-
-init();
 
